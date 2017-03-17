cbcf7bbf471fd1460eafdab256afd827d581239e
TANABE Takashi <narinari.t@gmail.com>
2017-03-17 11:16:07 +0900

    Bump up Halogen.

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -3,18 +3,19 @@ module Main where
 import Graphics.SignaturePad.Types
 import Graphics.SignaturePad.Halogen.Component as SignaturePad
 import Halogen as H
-import Halogen.HTML.Events.Indexed as HE
-import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Properties.Indexed as HP
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import CSS.Color (rgb)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
-import Data.Functor.Coproduct (Coproduct)
 import Data.Maybe (maybe, Maybe(..))
-import Halogen.Util (runHalogenAff, awaitBody)
-import Prelude (id, class Eq, class Ord, type (~>), Unit, unit, pure, bind, (=<<), ($), (==))
+import Data.Void (Void, absurd)
+import Halogen.Aff (HalogenEffects, runHalogenAff, awaitBody)
+import Halogen.VDom.Driver (runUI)
+import Prelude (class Eq, class Ord, type (~>), Unit, bind, const, id, pure, unit, ($), (=<<), (==))
 
-type MainEffects = H.HalogenEffects ()
+type MainEffects = HalogenEffects ()
 
 data Query a
   = SigClear SignaturePadSlot a
@@ -34,33 +35,30 @@ data SignaturePadSlot = SignaturePadSlot String
 derive instance signaturePadSlotOrd :: Ord SignaturePadSlot
 derive instance signaturePadSlotEq :: Eq SignaturePadSlot
 
-type StateP g = H.ParentState State SignaturePad.State Query SignaturePad.Query g SignaturePadSlot
-type QueryP = Coproduct Query (H.ChildF SignaturePadSlot SignaturePad.Query)
-type MainHTML g = H.ParentHTML SignaturePad.State Query SignaturePad.Query g SignaturePadSlot
+type MainHTML g = H.ParentHTML Query SignaturePad.Query SignaturePadSlot g
 type MainAff = Aff MainEffects
-type MainDSL g = H.ParentDSL State SignaturePad.State Query SignaturePad.Query g SignaturePadSlot
+type MainDSL g = H.ParentDSL State Query SignaturePad.Query SignaturePadSlot Void g
 
-
-ui :: H.Component (StateP MainAff) QueryP MainAff
-ui = H.parentComponent { render, eval, peek: Nothing }
+ui :: H.Component HH.HTML Query Unit Void MainAff
+ui = H.parentComponent { initialState: const initialState, render, eval, receiver: const Nothing }
   where
   padConfig = defaultConfig { backgroundColor = rgb 220 220 220 }
   render :: State -> MainHTML MainAff
   render state =
     HH.div_
     [ HH.div_
-      [ HH.slot slotA
-          \_ -> { component: SignaturePad.component
-                , initialState: SignaturePad.initialState
-                    { config = padConfig }
-                }
+      [ HH.slot
+        slotA
+        (SignaturePad.component $ Just padConfig)
+        unit
+        absurd
       , HH.button
-        [ HP.buttonType HP.ButtonButton
+        [ HP.type_ HP.ButtonButton
         , HE.onClick $ HE.input_ $ SigClear slotA
         ]
         [ HH.text "Clear" ]
       , HH.button
-        [ HP.buttonType HP.ButtonButton
+        [ HP.type_ HP.ButtonButton
         , HE.onClick $ HE.input_ $ SigCapture slotA
         ]
         [ HH.text "Capture" ]
@@ -68,13 +66,13 @@ ui = H.parentComponent { render, eval, peek: Nothing }
         [ HP.src $ maybe "" id state.sig1Data ]
       ]
     , HH.div_
-      [ HH.slot slotB
-          \_ -> { component: SignaturePad.component
-                , initialState: SignaturePad.initialState
-                    { config = padConfig { height = 300.0, width = 500.0 }}
-                }
+      [ HH.slot
+        slotB
+        (SignaturePad.component $ Just padConfig { height = 300.0, width = 500.0 })
+        unit
+        absurd
       , HH.button
-        [ HP.buttonType HP.ButtonButton
+        [ HP.type_ HP.ButtonButton
         , HE.onClick $ HE.input_ $ SigClear slotB
         ]
         [ HH.text "Clear" ]
@@ -92,8 +90,8 @@ ui = H.parentComponent { render, eval, peek: Nothing }
     isEmpty <- H.query slot $ H.request SignaturePad.IsEmpty
     sigData <- H.query slot $ H.request SignaturePad.Get
     maybe (pure unit) (if _ then pure unit else H.modify _ { sig1Data = id =<< sigData }) $ id =<< isEmpty
-    
+
     pure next
- 
+
 main :: Eff MainEffects Unit
-main = runHalogenAff $ H.runUI ui (H.parentState initialState) =<< awaitBody
+main = runHalogenAff $ runUI ui unit =<< awaitBody
--- src/Graphics/SignaturePad/Halogen/Component.purs
+++ src/Graphics/SignaturePad/Halogen/Component.purs
@@ -1,20 +1,20 @@
 module Graphics.SignaturePad.Halogen.Component where
 
 import Halogen as H
-import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Properties.Indexed as HP
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Control.Monad.Aff.Class (class MonadAff)
-import Control.Monad.Aff.Free (class Affable)
 import Control.Monad.Except (runExcept)
 import DOM (DOM)
 import DOM.HTML.Types (HTMLCanvasElement)
 import Data.Either (either)
-import Data.Foreign (toForeign)
+import Data.Foldable (for_)
 import Data.Foreign.Class (read)
-import Data.Maybe (Maybe(..), maybe)
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
 import Data.Traversable (sequence)
+import Data.Void (Void)
 import Graphics.SignaturePad (isEmpty, Config, SignaturePad, mkSignaturePad, defaultConfig, fromDataURL, toDataURL, clear, on, off)
-import Prelude (type (~>), pure, void, unit, bind, const, not, flip, (<$>), (<<<), (=<<), ($), ($>))
+import Prelude (type (~>), Unit, bind, const, flip, not, pure, unit, void, ($), ($>), (<#>), (<$>), (<<<), (>>=), (>>>))
 
 type SignaturePadEffects eff = (dom :: DOM | eff)
 
@@ -34,63 +34,65 @@ data Query a
   | Get (Maybe String -> a)
   | IsEmpty (Maybe Boolean -> a)
 
-initialState :: State
-initialState = 
+initialState :: Maybe Config -> State
+initialState config =
   { element: Nothing
   , signaturePad: Nothing
-  , config: defaultConfig
+  , config: fromMaybe defaultConfig config
   , enabled: false
   }
 
 component
   :: forall m eff
   . ( MonadAff (SignaturePadEffects eff) m
-    , Affable (SignaturePadEffects eff) m
     )
-  => H.Component State Query m
-component = H.lifecycleComponent
-  { render
+  => Maybe Config
+  -> H.Component HH.HTML Query Unit Void m
+component config = H.lifecycleComponent
+  { initialState: const $ initialState config
+  , render
   , eval
+  , receiver: const Nothing
   , initializer: Just (H.action Init)
   , finalizer: Nothing
   }
+
   where
   render :: State -> H.ComponentHTML Query
   render _ =
     HH.canvas
-    [ HP.ref (\elm -> H.action $ SetElement $ (either (const Nothing ) pure <<< runExcept <<< read <<< toForeign) =<< elm) ]
+    [ HP.ref (H.RefLabel "signature") ]
 
-  eval :: Query ~> H.ComponentDSL State Query m
+  eval :: Query ~> H.ComponentDSL State Query Void m
   eval (SetElement elm next) = H.modify (_ { element = elm}) $> next
   eval (Init next) = do
-    elm <- H.gets _.element
-    config <- H.gets _.config
-    case elm of
-      Nothing -> pure unit
-      Just el' -> do
-        signaturePad <- H.fromEff $ mkSignaturePad el' config
+    elm <- H.getRef (H.RefLabel "signature") <#> (_ >>= read >>> runExcept >>> either (const Nothing ) Just)
+    H.modify _ { element = elm }
+    cfg <- H.gets _.config
+    for_ elm \el' -> do
+        signaturePad <- H.liftEff $ mkSignaturePad el' cfg
         H.modify (_ { signaturePad = Just signaturePad, enabled = true })
         pure unit
     pure next
   eval (Clear next) = do
     signaturePad <- H.gets _.signaturePad
-    maybe (pure unit) (void) $ H.fromEff <<< clear <$> signaturePad
+    maybe (pure unit) (void) $ H.liftEff <<< clear <$> signaturePad
     pure next
   eval (Toggle next) = do
     H.modify \s -> s { enabled = not s.enabled }
     signaturePad <- H.gets _.signaturePad
     enabled <- H.gets _.enabled
-    maybe (pure unit) (void) $ H.fromEff <<< (if enabled then on else off) <$> signaturePad
+    maybe (pure unit) (void) $ H.liftEff <<< (if enabled then on else off) <$> signaturePad
     pure next
   eval (Load signData next) = do
     signaturePad <- H.gets _.signaturePad
-    maybe (pure unit) (void) $ H.fromEff <<< flip fromDataURL signData <$> signaturePad
+    maybe (pure unit) (void) $ H.liftEff <<< flip fromDataURL signData <$> signaturePad
     pure next
   eval (Get getter) = do
     signaturePad <- H.gets _.signaturePad
-    url <- sequence $ H.fromEff <<< flip toDataURL "image/png" <$> signaturePad
+    url <- sequence $ H.liftEff <<< flip toDataURL "image/png" <$> signaturePad
     pure $ getter url
   eval (IsEmpty getter) = do
     signaturePad <- H.gets _.signaturePad
-    res <- sequence $ H.fromEff <<< isEmpty <$> signaturePad
+    res <- sequence $ H.liftEff <<< isEmpty <$> signaturePad
     pure $ getter res
